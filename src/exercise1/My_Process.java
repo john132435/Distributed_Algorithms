9c59f944ae2e4f8a328a272d101e91166aaf6a36
treloz_fish <jschistos@gmail.com>
2017-11-20 18:45:06 +0100

    + costas code

--- src/exercise1/My_Process.java
+++ src/exercise1/My_Process.java
@@ -1,5 +1,10 @@
 package exercise1;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.rmi.AlreadyBoundException;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
@@ -12,10 +17,28 @@ public class My_Process implements Runnable, PrInter {
 	public static int timestamp = 1;
 	public static int process_id;
 	public static LinkedList<Message> Received_Messages = new LinkedList<Message>();
+	public static LinkedList<Message> fifo;
 	
 	public My_Process(){}; 
 	
 	public static void main(String args[]) {
+		
+		LinkedList<Message> ls;
+		 
+		  try {
+		  
+		   System.out.print("File to check is "+args[2]);
+		   String f = "C:/Users/Kostas/workspace/rmi/bin/exercise1/Messages"+args[2]+".txt";
+		   System.out.print("String to check is " + f);
+		         File file = new File(f);
+		         ls = LoadMessages(file);
+		         fifo = ls;
+		         for (int i=0; i<fifo.size(); i++) System.out.print("INDEX"+i);
+		       }
+		       catch(IOException ioe) {
+		          ioe.printStackTrace();
+		       }
+		       System.out.print("SIZE = "+fifo.size());
 		
 		System.out.println("P_id:"+args[2]);
 	
@@ -88,5 +111,45 @@ public class My_Process implements Runnable, PrInter {
 		}
 		return "Received";
 	}
+	
+	public static LinkedList<Message> LoadMessages(File f) throws IOException {
+		   int index;
+		   String ts, pid;
+		   int t,p;
+		   Message m;
+		   LinkedList<Message> list = new LinkedList<Message>();
+		   FileInputStream fis = new FileInputStream(f);
+		   
+		   //Construct BufferedReader from InputStreamReader
+		   BufferedReader br = new BufferedReader(new InputStreamReader(fis));
+		   
+		   String line = null;
+		   while ((line = br.readLine()) != null) {
+		    try{
+		    if (line==null) 
+		    	System.out.println("FOUND");
+		     index = line.indexOf(',');
+		     if (index>0){ 
+		     ts = line.substring(0,index);
+		     pid = line.substring(index+1,line.length());
+		    
+		    t = Integer.parseInt(ts);
+		    p = Integer.parseInt(pid);
+		    System.out.println("timestamp = "+t+",pid = "+p);
+		    
+		    m = new Message(t, p,"Message from "+p,false);
+		    //m.print();
+		    list.add(m);
+		     }
+		    }catch (Exception e) {
+		              System.err.println("Load exception: " + e.toString());
+		              e.printStackTrace();
+		          }
+		   }
+		   
+		   br.close();
+		   
+		   return list;
+		  }
 	 
 }
